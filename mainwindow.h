bb6d5c0cfdb1d1671d2c9311ff21c19c85b1be29
Stanislav Zaleski <sigmaray@gmail.com>
2023-07-15 23:23:23 +0600

    ~

--- mainwindow.h
+++ mainwindow.h
@@ -3,10 +3,16 @@
 
 #include <QMainWindow>
 #include <QKeyEvent>
+#include <QtWidgets/QCheckBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+struct coordinate{             // Structure declaration
+  int x = 0;         // Member (int variable)
+  int y = 0;   // Member (string variable)
+};
 
 class MainWindow : public QWidget
 {
@@ -18,6 +24,14 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    void render();
+
+    std::vector<std::vector<coordinate>> matrix;
+    std::vector<std::vector<QCheckBox *>> checkboxes;
+
+
+    coordinate current;
 
 protected:
     virtual void keyPressEvent( QKeyEvent *  e);
