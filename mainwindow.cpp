e121c2aa2edfa55576244b4daaefc71e145c916b
Stanislav Zaleski <sigmaray@gmail.com>
2023-07-15 22:06:50 +0600

    ~

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,9 @@
 #include <QtWidgets/QCheckBox>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QMessageBox>
+
+#define BOARD_SIZE 5
 
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
@@ -12,10 +15,11 @@ MainWindow::MainWindow(QWidget *parent)
 
 
     QVBoxLayout *vlayout = new QVBoxLayout;
-    for ( int i = 0; i < 5; i++ ) {
+    for ( int i = 0; i < BOARD_SIZE; i++ ) {
         QHBoxLayout *hlayout = new QHBoxLayout;
-        for ( int i = 0; i < 5; i++ ) {
-            QCheckBox *checkbox = new QCheckBox(QString::number(i));
+        for ( int j = 0; j < BOARD_SIZE; j++ ) {
+            QCheckBox *checkbox = new QCheckBox(QString::number(j) + ":" + QString::number(i));
+            checkbox->setEnabled(false);
             hlayout->addWidget(checkbox);
         }
         vlayout->addLayout(hlayout);
@@ -25,6 +29,22 @@ MainWindow::MainWindow(QWidget *parent)
 
 //    layout->addWidget(checkbox);
 
+}
+
+void MainWindow::keyPressEvent( QKeyEvent * e)
+{
+   QMessageBox msgBox;
+
+   msgBox.setText(QString::number(e->key()));
+   msgBox.exec();
+
+    if(e->key()==Qt::Key_Left)
+    {
+        msgBox.setText("OK");
+        msgBox.exec();
+    }
+
+//    QTextEdit::keyPressEvent(e);
 }
 
 MainWindow::~MainWindow()
